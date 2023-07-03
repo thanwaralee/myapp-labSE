aca5a87af17a8c96d0586e6056b410359e5ec16b
thanwaralee <thanwaralee.t@ku.th>
2023-07-03 21:18:43 +0700

    update Class Z (fish)

--- src/main/java/myapp/Fish.java
+++ src/main/java/myapp/Fish.java
@@ -2,5 +2,19 @@ package myapp;
 
 public class Fish {
     String name;
+    int age;
+    int distance;
     float swim;
+
+    public Fish(String name, float swim,int age) {
+        this.name = name;
+        this.swim = swim;
+        this.age = age;
+    }
+
+    public void Swim_1min(){
+        this.swim += 1.5;
+        this.distance += 1;
+    }
+
 }
